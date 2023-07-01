1ef7c74f3a290de1319d65c2d3ea858046d5e8bf
miqueas <bagente24@gmail.com>
2023-07-01 18:28:49 -0300

    se corrigio errors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // Imports
+require("dotenv").config();
+const port = process.env.PORT || 4000;
 const cors = require("cors");
 const express = require("express");
 const dotenv = require("dotenv");
@@ -6,16 +8,16 @@ const path = require("path");
 
 const app = express();
 // El método config de dotenv permite leer variables de entorno desde un archivo .env
-require("dotenv").config();
+
 // Middlewares
 // TODO: Implementar middlewares
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
-app.use("/api", require("./routes/reserva.routes"));
+// // Routes
+// app.use("/api", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(4000, () => console.log(`Server on port ${4000}`));
+app.listen(port, () => console.log(`Server on port ${port}`));
